55eb3cd55cfab0a210ca2865f151253de7b17169
Derkysan <derkysan19@gmail.com>
2023-02-01 23:58:24 -0300

    feat: semantic-versioning added

---
+++ dist/components/MyLabel.d.ts
@@ -0,0 +1,29 @@
+/// <reference types="react" />
+import './myLabel.css';
+export interface MyLabelProps {
+    /**
+     * Set text
+     */
+    label: string;
+    /**
+     * Set text size
+     */
+    size: 'normal' | 'h1' | 'h2' | 'h3';
+    /**
+     * Set Uppercase
+     */
+    allCaps?: boolean;
+    /**
+     * Set color theme
+     */
+    color?: 'primary' | 'secondary' | 'tertiary';
+    /**
+     * Set custom color
+     */
+    fontColor?: string;
+    /**
+     * Set custom color
+     */
+    backgroundColor?: string;
+}
+export declare const MyLabel: ({ label, size, allCaps, color, fontColor, backgroundColor }: MyLabelProps) => JSX.Element;
